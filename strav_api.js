0d9c92497c604701825813d4ff48bea46a42cd65
PCorrea1 <pcorrea6@gatech.edu>
2022-08-02 21:30:46 -0400

    update

---
+++ strav_api.js
@@ -0,0 +1,11 @@
+
+
+function getActivities(){
+
+    const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=3ce13a7350ab004be138b57afa5a2e48841964ab"
+    fetch(activities_link)
+        .then((res) => console.log(res))
+
+}
+
+getActivities()
