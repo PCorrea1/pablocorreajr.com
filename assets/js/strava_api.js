6b8106e5893f67bf034d55684900b71196002539
PCorrea1 <pcorrea6@gatech.edu>
2022-08-03 22:55:04 -0400

    update

--- assets/js/strava_api.js
+++ assets/js/strava_api.js
@@ -1,16 +1,16 @@
 
 
-
 const auth_link = "https://www.strava.com/oauth/token"
-function getActivities(){
 
-    const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=d3e4d2f0362eba72523b6c693106bf6775d17dbb"
-    fetch(activities_link)
-        .then((res) => console.log(res.json()))
+//function getActivities(){
 
-}
+   // const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=d3e4d2f0362eba72523b6c693106bf6775d17dbb"
+   // fetch(activities_link)
+  //      .then((res) => console.log(res.json()))
 
-getActivities()
+//}
+
+//getActivities()
 
 function reAuthorize(){
     fetch(auth_link,{
