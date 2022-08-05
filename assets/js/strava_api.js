e7f92753fc690b785b11643b8b07b03d4df7ad1c
PCorrea1 <pcorrea6@gatech.edu>
2022-08-05 16:31:11 -0400

    send run 2 info to site

--- assets/js/strava_api.js
+++ assets/js/strava_api.js
@@ -2,32 +2,32 @@
 
 const auth_link = "https://www.strava.com/oauth/token"
 
-//function getActivities(){
+function getActivities(){
 
-   // const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=d3e4d2f0362eba72523b6c693106bf6775d17dbb"
-   // fetch(activities_link)
-  //      .then((res) => console.log(res.json()))
+    const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=d3e4d2f0362eba72523b6c693106bf6775d17dbb"
+    fetch(activities_link)
+        .then((res) => console.log(res.json()))
 
-//}
-
-//getActivities()
-
-function reAuthorize(){
-    fetch(auth_link,{
-        method:'post',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-
-        body: JSON.stringify({
-
-            client_id: '91142',
-            client_secret: '217274bcb34de972f7037193069cd966293f56f0',
-            refresh_token: 'a724bf6544b3865d659f8c16fce7f5cf3ae3ef5a',
-            grant_type: 'refresh_token'
-        })
-    }).then((res) => console.log(res.json()))
 }
 
-reAuthorize()
+getActivities()
+
+//function reAuthorize(){
+  //  fetch(auth_link,{
+    //    method:'post',
+      //  headers: {
+        //    'Accept': 'application/json, text/plain, */*',
+          //  'Content-Type': 'application/json'
+        //},
+
+        //body: JSON.stringify({
+
+          //  client_id: '91142',
+            //client_secret: '217274bcb34de972f7037193069cd966293f56f0',
+            //refresh_token: 'a724bf6544b3865d659f8c16fce7f5cf3ae3ef5a',
+            //grant_type: 'refresh_token'
+        //})
+    //}).then((res) => console.log(res.json()))
+//}
+
+//reAuthorize()
